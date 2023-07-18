55b39e6ff4bf1299929d6773763193a74cbb696a
Choel_ramos <joel4785ramos@gmail.com>
2023-07-17 22:54:22 -0300

    Testando mais algumas forma de implementar exceções

--- basic-exceptions/Erros-e-excecoes/basic-exceptions.py
+++ basic-exceptions/Erros-e-excecoes/basic-exceptions.py
@@ -14,10 +14,38 @@ Exceptions exemples:"""
 """Handling Exceptions!
 See to a simgle exemple bellow"""
 
-while True:
+#while True:
+#   try:
+#        x = int(input("Please enter a number: "))
+#        break
+#    except ValueError:
+#        print("Opa! That was no valid number. Try again...")
+
+
+"""The most common pattern for handling Exception is to print or log the exception
+and then re-raise it (allowing a caller to handle the excepition as well)"""
+
+import sys
+#try:
+#    f = open('myfile.txt')
+#    s = f.readline()
+#    i = int(s.strip())
+#except OSError as err:
+#    print("OS error:", err)
+#except ValueError:
+#    print("Could not convert data to an integer.")
+#except Exception as err:
+#    print(f"unexpected {err}, {type(err)=}")
+
+"""The else clause. It is useful for code that must be executed if the try clause
+does note raise on exception. For exemple:"""
+for arg in sys.argv[1:]:
     try:
-        x = int(input("Please enter a number: "))
-        break
-    except ValueError:
-        print("Opa! That was no valid number. Try again...")
+        f = open(arg, 'r')
+    except OSError:
+        print("Cannot open", arg)
+    else:
+        print(arg, 'has', len(f.readline()), 'lines')
+        f.close()
+
 
