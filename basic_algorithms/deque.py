6723e999beacd561bfe15ba06cea904847a2822c
Choel_ramos <joel4785ramos@gmail.com>
2023-07-26 12:35:45 -0300

    Implementando minha própria linked list

--- basic_algorithms/deque.py
+++ basic_algorithms/deque.py
@@ -46,3 +46,43 @@ history.appendleft("https://google.com")
 history.appendleft("https://youtube.com")
 history.appendleft("https://stackoverflow.com")
 print(history)
+
+# My own linked list
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return self.data
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+# An exemple using the aboe classes:
+
+
+llist = LinkedList()
+print(llist)
+first_node = Node("a")
+llist.head = first_node
+print(llist)
+
+second_node = Node("b")
+third_node = Node("c")
+first_node.next = second_node
+second_node.next = third_node
+print(llist)
