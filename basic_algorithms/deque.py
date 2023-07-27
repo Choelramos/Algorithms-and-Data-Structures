1e4d39d869726f03e22f1b792712895fd10b4834
Choel_ramos <joel4785ramos@gmail.com>
2023-07-27 16:26:12 -0300

    Métodos para criar uma Linked list mais rapidamente

--- basic_algorithms/deque.py
+++ basic_algorithms/deque.py
@@ -20,7 +20,7 @@ print(llist)
 
 llist.popleft()
 print(llist)
-print()
+print('\n')
 
 # Queues with linked lists
 
@@ -36,6 +36,8 @@ print(queue)
 
 queue.popleft()
 print(queue)
+print('\n')
+
 
 """Stacks
 In this example, i created an empty history object, ans every time the user visited a nes site,
@@ -46,10 +48,10 @@ history.appendleft("https://google.com")
 history.appendleft("https://youtube.com")
 history.appendleft("https://stackoverflow.com")
 print(history)
-
+print('\n')
 # My own linked list
 
-
+"""
 class Node:
     def __init__(self, data):
         self.data = data
@@ -71,7 +73,7 @@ class LinkedList:
             node = node.next
         nodes.append("None")
         return " -> ".join(nodes)
-
+            
 # An exemple using the aboe classes:
 
 
@@ -85,4 +87,46 @@ second_node = Node("b")
 third_node = Node("c")
 first_node.next = second_node
 second_node.next = third_node
+print(llist)
+"""
+
+# In exemple below I can create linked lists faster
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return self.data
+
+
+class LinkedList:
+    def __init__(self, nodes=None):
+        self.head = None
+        if nodes is not None:
+            node = Node(data=nodes.pop(0))
+            self.head = node
+            for elem in nodes:
+                node.next = Node(data=elem)
+                node = node.next
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+
+llist = LinkedList(["0", "1", "2", "3", "4", "5"])
 print(llist)
