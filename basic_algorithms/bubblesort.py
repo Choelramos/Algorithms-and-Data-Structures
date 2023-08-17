9eab820f6100ae330e8110f09ee41271c1b55026
Choel_ramos <joel4785ramos@gmail.com>
2023-08-17 15:36:33 -0300

    Implementando outro método e testando via timing_code o algoritmo

--- basic_algorithms/bubblesort.py
+++ basic_algorithms/bubblesort.py
@@ -1,4 +1,8 @@
+import timing_code as tm
+from random import randint
+
 # Ordenação pelo método bolha (bubblesort)
+"""
 def trocar(seq, i):
     aux = seq[i]
     seq[i] = seq[i + 1]
@@ -16,3 +20,36 @@ while troca:
             trocar(seq, i)
             troca = 1
 print(seq)
+"""
+
+
+# Another method of Bubble sort algorithm
+def bubble_sort(array):
+    n = len(array)
+
+    for x in range(n):
+        already_sorted = True
+
+        for y in range(n - x - 1):
+            if array[y] > array[y + 1]:
+                array[y], array[y + 1] = array[y + 1], array[y]
+                already_sorted = False
+
+        if already_sorted:
+            break
+
+    return array
+
+
+# Testing using timing_code imported
+if __name__ == "__main__":
+    array = [randint(0, 1000) for i in range(tm.ARRAY_LENGTH)]
+
+    tm.run_sorting_algorithm(algorithm="bubble_sort", array=array)
+
+
+# My implementation:
+print()
+arr = [5, 3, 8, 9, 1, 2]
+i = bubble_sort(arr)
+print(i)
---
+++ tests/calc.py
@@ -0,0 +1,8 @@
+# A simple module
+def add(x, y):
+    return x+y
+
+
+def subtract(x, y):
+    return x-y
+
